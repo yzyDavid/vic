985d729f98ab30a9a55de4ec99bc005f18e1e3eb
yzyDavid <yzyDavid@qq.com>
2016-03-13 09:52:54 +0000

    add readme

---
+++ readme.md
@@ -0,0 +1,4 @@
+# vic
+
+##a plain text editor imitating minimal functions of vi
+##currently designed to perform only under POSIX OS
--- src/termios_set.h
+++ src/termios_set.h
@@ -9,8 +9,22 @@
 #ifndef VIC_TERMIOS_SET_H
 #define VIC_TERMIOS_SET_H
 
+#include "draw_ui.h"
+
 int init_display_back();
 int disable_display_back();
 int enable_display_back();
+
+inline int hide_cursor()
+{
+    printf(HIDE_CURSOR);
+    return 0;
+}
+
+inline int show_cursor()
+{
+    printf(SHOW_CURSOR);
+    return 0;
+}
 
 #endif //VIC_TERMIOS_SET_H
