b1d8b448a7d01e841860311a7be8c6b410d15eac
yzyDavid <yzyDavid@qq.com>
2016-03-21 18:52:26 +0800

    added a test program to check the value of getch(). the getch fuction is declared in conio.h.

---
+++ src/test/win_test_main.c
@@ -0,0 +1,16 @@
+//
+// Created by yzy on 3/21/2016.
+//
+
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    while (1)
+    {
+        printf("%x\n", getch());
+    }
+    return 0;
+}
